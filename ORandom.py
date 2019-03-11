d2b3472a816beb5ccbed4292a26b6f8c8c466ff0
Sunny Kumar <iamthesunnykumar@gmail.com>
2019-03-11 19:35:04 +0530

    Added class based ORandom

--- ORandom.py
+++ ORandom.py
@@ -1,5 +1,9 @@
 import random
 
+"""
+Functional random strings
+"""
+
 alphas = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 upper_alphas = [alpha.upper() for alpha in alphas]
@@ -20,10 +24,36 @@ def _random(bits):
     
     return _random
 
+"""
+Class Based random strings
+"""
+
+class ORandom:
+    alphas = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+    upper_alphas = [alpha.upper() for alpha in alphas]
+
+    nums = ['0','1','2','3','4','5','6','7','8','9']
+
+    spec = ['_', '-']
+
+    random_salt = [alphas, nums, spec, upper_alphas]
+
+    def create_random(self, bit):
+        _random = []
+
+        for i in range(bit):
+            _random.append(random.choice(random.choice(self.random_salt)))
+
+        _random = "".join(_random)
+
+        return _random
+
+
 if __name__ == "__main__":
-    f= []
 
     def _get_rand():
+        f= []
         for i in range(100):
             f.append(_random(11))
         return f
@@ -33,3 +63,13 @@ if __name__ == "__main__":
     print(f)
     print(len(f))
     print(len(set(f)))
+
+    def class_rand():
+        f= []
+        for i in range(100):
+            f.append(ORandom().create_random(3))
+        return f
+
+    data_rand = class_rand()
+
+    print(data_rand)
